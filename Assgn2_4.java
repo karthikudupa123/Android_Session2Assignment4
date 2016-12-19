f38ae21569488b06eabf74bc6f5991c09b39363d
karthikudupa123 <karthikudupa123@gmail.com>
2016-12-19 12:30:54 +0530

    commit

---
+++ Assgn2_4.java
@@ -0,0 +1,16 @@
+public class Assgn2_4
+{
+ public static void main(String []args)
+ {
+  int a,b,c;
+  a=49;
+  b=50;
+  c=96;
+  if(a>b&&a>c)
+  System.out.println("a="+a+" is greatest");
+  else if(b>a&&b>c)
+  System.out.println("b="+b+" is greatest");
+  else
+  System.out.println("c="+c+" is greatest");
+  }
+}
